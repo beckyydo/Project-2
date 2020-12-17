021876a8a40464336266c8e7ea9be1dbdbc2a09a
haniehbabaee <70447525+haniehbabaee@users.noreply.github.com>
2020-12-17 18:22:55 -0500

    update

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-connection_string= "ofiglsqd:vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe@otto.db.elephantsql.com:5432/ofiglsqd"
+connection_string="ofiglsqd:vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe@otto.db.elephantsql.com:5432/ofiglsqd"
